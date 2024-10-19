fc5fcfb7d663a8cccbb1f5d8a17b12005984db93
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-10-19 17:30:05 -0300

    ss

--- proj_dp/settings.py
+++ proj_dp/settings.py
@@ -126,14 +126,14 @@ MEDIA_URL = '/media/'
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STORAGES = {
 #     # ...
