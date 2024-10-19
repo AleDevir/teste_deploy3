41e668213dd215bf97aac6628ffe264747a0aa7b
aledev-lab <aledev.b2b@gmail.com>
2024-10-19 16:30:58 -0300

    Update settings.py

--- proj_dp/settings.py
+++ proj_dp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#d-7!9+p)vz8vtkzu90ktql7^7tl_r)%g23s3^v3xe4l1cmd6-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
