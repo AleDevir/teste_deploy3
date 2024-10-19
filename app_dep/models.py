fe609ff7c474ca520d8c14a1182e710ea290b800
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-10-19 18:59:08 -0300

    1

--- app_dep/models.py
+++ app_dep/models.py
@@ -5,8 +5,10 @@ class Categoria(models.Model):
     Categoria
     '''
     nome = models.CharField('Categoria', max_length=30)
+    
     criado_em = models.DateField(auto_now_add=True)
     imagem = models.ImageField(upload_to='', blank=True)
+    slug = models.SlugField(default="", null=False)
 
     
     def __str__(self):
