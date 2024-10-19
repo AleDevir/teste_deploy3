3d46ed1c1fc5bf5c2b90f6f3f5ddc7734fafd832
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-10-19 17:12:17 -0300

    stati

--- proj_dp/settings.py
+++ proj_dp/settings.py
@@ -130,7 +130,7 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -141,6 +141,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
